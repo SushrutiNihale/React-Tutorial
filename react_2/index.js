14f3038f9c57901bfdb707a2923f734441130b10
Sushruti Nihale <sushrutin6@gmail.com>
2022-03-29 19:23:35 +0530

    first commit

---
+++ react_2/index.js
@@ -0,0 +1,7 @@
+
+let elem = <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+</ul>
+
+ReactDOM.render(elem, document.getElementById("root"));
---
+++ react_3/index.js
@@ -0,0 +1,5 @@
+function MainContent() {
+    return <h1>This is React file 3</h1>
+}
+
+ReactDOM.render(<MainContent />, document.getElementById("root"));
